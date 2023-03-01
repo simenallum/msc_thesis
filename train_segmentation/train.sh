10967cc3c768af00832d280d4b2a7f10a2a52748
Simen Allum <simensal@stud.ntnu.no>
2023-03-01 18:37:29 +0100

    Initial seg results

---
+++ train_segmentation/train.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Load the JSON configuration file
+CONFIG_FILE=$1
+CONFIG=$(cat $CONFIG_FILE)
+
+# Parse the JSON configuration
+EPOCHS=$(echo $CONFIG | jq -r '.epochs')
+BATCH_SIZE=$(echo $CONFIG | jq -r '.batch_size')
+LEARNING_RATE=$(echo $CONFIG | jq -r '.learning_rate')
+LOAD=$(echo $CONFIG | jq -r '.load')
+SCALE=$(echo $CONFIG | jq -r '.scale')
+VALIDATION=$(echo $CONFIG | jq -r '.validation')
+AMP=$(echo $CONFIG | jq -r '.amp')
+BILINEAR=$(echo $CONFIG | jq -r '.bilinear')
+CLASSES=$(echo $CONFIG | jq -r '.classes')
+
+# Construct the command string
+CMD="python3 Pytorch-UNet/train.py"
+if [ ! -z "$EPOCHS" ]; then CMD+=" --epochs $EPOCHS"; fi
+if [ ! -z "$BATCH_SIZE" ]; then CMD+=" --batch-size $BATCH_SIZE"; fi
+if [ ! -z "$LEARNING_RATE" ]; then CMD+=" --learning-rate $LEARNING_RATE"; fi
+if [ ! -z "$LOAD" ]; then CMD+=" --load $LOAD"; fi
+if [ ! -z "$SCALE" ]; then CMD+=" --scale $SCALE"; fi
+if [ ! -z "$VALIDATION" ]; then CMD+=" --validation $VALIDATION"; fi
+if [ ! -z "$AMP" ]; then CMD+=" --amp"; fi
+if [ ! -z "$BILINEAR" ]; then CMD+=" --bilinear"; fi
+if [ ! -z "$CLASSES" ]; then CMD+=" --classes $CLASSES"; fi
+
+# Execute the command
+eval $CMD
