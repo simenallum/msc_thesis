7e2438655d261363fbf2426ef598e5add2d0c5ca
simensal <simensal@stud.ntnu.no>
2023-03-13 21:21:23 +0100

    Renamed variables

--- water_segmentation/scripts/segmentation_master_utils/utils.py
+++ water_segmentation/scripts/segmentation_master_utils/utils.py
@@ -45,42 +45,42 @@ def find_safe_locations_in_mask(mask: np.array, safe_dist_px: int) -> tuple:
     pass
 
 
-def find_safe_areas(image, x):
-    # Define a sliding window of size x
-    window = np.ones((x, x), dtype=np.uint8)
+def find_safe_areas(image: np.ndarray, window_size: int, stride: int) -> np.ndarray:
+    # Define a sliding window of size window_size
+    window = np.ones((window_size, window_size), dtype=np.uint8)
 
-    binary_img = np.where(image > 0, 0, 1)
+    binary_img = np.where(image > 0, 0, 1)  # Invert image and make binary
     center = np.array(binary_img.shape) / 2
 
-    # Crop a subarray centered at the image center and of size x times x
-    center_window = binary_img[int(center[0]-x/2):int(center[0]+x/2), int(center[1]-x/2):int(center[1]+x/2)]
+    # Crop a subarray centered at the image center and of size window_size times window_size
+    center_window = binary_img[
+        int(center[0] - window_size / 2):int(center[0] + window_size / 2),
+        int(center[1] - window_size / 2):int(center[1] + window_size / 2)
+    ]
 
     # Check if the center window is safe
-    if np.sum(np.multiply(center_window, window)) == x**2:
+    if np.sum(np.multiply(center_window, window)) == window_size ** 2:
         return np.array([center])
 
-    # Set the stride
-    stride = 10
-
-    # Apply the kernel to the image with the specified stride
+    # Split the binary image into windows
     windows = view_as_windows(binary_img, window.shape, step=stride)
 
     safe_loc = []
     for i in range(len(windows)):
         for j in range(len(windows[i])):
             res = np.multiply(windows[i, j], window)
-            if np.sum(res) == x**2:
-                safe_loc.append([(i*stride)+(x//2), (j*stride)+(x//2)])
-            
+            if np.sum(res) == window_size ** 2:
+                safe_loc.append([(i * stride) + (window_size // 2), (j * stride) + (window_size // 2)])
+
     safe_loc = np.array(safe_loc)
 
     # Calculate the distances of the safe windows to the center of the image
     distances = np.linalg.norm(safe_loc - center, axis=1)
-    
+
     # Sort the safe windows by their distance to the center of the image
     sorted_indices = np.argsort(distances)
     sorted_positions = safe_loc[sorted_indices]
-    
+
     return sorted_positions
 
 
@@ -91,12 +91,13 @@ if __name__ == '__main__':
     focal_length = 932.8763
     drone_alt = 60
     safe_metric_dist = 5
+    stride = 10
 
     px_safe_dist = convert_save_dist_to_px(focal_length, drone_alt, safe_metric_dist)
 
     # Example usage with a square of size 10 pixels
     img = cv2.imread("/home/simenallum/Desktop/test10_0.png", 0)
-    sorted = find_safe_areas(img, px_safe_dist)
+    sorted = find_safe_areas(img, px_safe_dist, stride)
     plt.imshow(img)
     plt.plot(sorted[0][1], sorted[0][0], 'o', ms=3, c='r')
     plt.plot(img.shape[0] // 2, img.shape[0] // 2, 'o', ms=3, c='g')
