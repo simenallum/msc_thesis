112faaf2d0999d5c6326f29d348df8e8a14c57d4
Simen Allum <simensal@stud.ntnu.no>
2023-03-29 15:47:06 +0200

    New DF sync function

---
+++ offline_utils/analysis_and_plotting/IMPORTS_AND_FUNCTIONS.py
@@ -0,0 +1,160 @@
+import bagpy
+from bagpy import bagreader
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+from functools import reduce
+from scipy.spatial.transform import Rotation
+import ipympl
+
+%matplotlib widget
+
+sns.set_theme()
+# LaTex must be installed for this to work
+# sudo apt-get install dvipng texlive-latex-extra texlive-fonts-recommended cm-super
+
+plt.rcParams['text.usetex'] = True
+plt.rc('text.latex', preamble=r'\usepackage{bm}')
+
+def drop_columns_in_df(df):
+    return df.drop(columns=["header.seq", "header.stamp.secs", "header.stamp.nsecs", "header.frame_id"])
+
+def rename_columns(df, new_colum_names: list):
+    column_names = ['Time'] + new_colum_names
+    
+    df.columns= column_names
+
+    return df
+
+def normalize_df_time(df):
+    start_time = df['Time'].iloc[0]
+
+    df['Time'] -= start_time
+
+    return df
+
+def sync_dfs_based_on_time(dataframes: list):
+    import math
+    earliest_time = math.inf
+    for frame in dataframes:
+        if frame.iloc[0]["Time"] < earliest_time:
+            earliest_time = frame.iloc[0]["Time"]
+
+    for frame in dataframes:
+        frame['Time'] -= earliest_time
+
+    return dataframes
+
+def df_from_pose_to_euler(df):
+    quat_df = df[['pose.orientation.x', 'pose.orientation.y', 'pose.orientation.z', 'pose.orientation.w']]
+    rot = Rotation.from_quat(quat_df)
+    rot_euler = rot.as_euler('xyz', degrees=True)
+    euler_df = pd.DataFrame(data=rot_euler, columns=['roll', 'pitch', 'yaw'])
+
+    conc = [df, euler_df]
+
+    df_gt_euler = pd.concat(
+        conc,
+        axis=1,
+        join="outer",
+        ignore_index=False,
+        keys=None,
+        levels=None,
+        names=None,
+        verify_integrity=False,
+        copy=True,
+    )
+
+    df_gt_euler = drop_columns_in_df(df_gt_euler)
+    df_gt_euler.drop(columns=["pose.position.x", "pose.position.y", "pose.position.z", "pose.orientation.x", "pose.orientation.y","pose.orientation.z","pose.orientation.w"], inplace=True)
+
+    return df_gt_euler
+
+def Mask_Or_Drop_Successive_Identical_Values(df, drop=False, 
+                                             keep_first=True,
+                                             axis=0):
+
+    '''
+    Input:
+    df should be a pandas.DataFrame of a a pandas.Series
+    Output:
+    df of ts with masked or dropped values
+    '''
+    
+    # Mask keeping the first occurrence
+    if keep_first:
+        df = df.mask(df.shift(1) == df)
+    # Mask including the first occurrence
+    else:
+        df = df.mask((df.shift(1) == df) | (df.shift(-1) == df))
+
+    # Drop the values (e.g. rows are deleted)    
+    if drop:
+        return df.dropna(axis=axis)        
+    # Only mask the values (e.g. become 'NaN')
+    else:
+        return df
+
+def get_np_arr_without_nan(df_list, datacols_list, remove_identicals=True):
+    np_column = np.array(["Time"] + datacols_list)
+    for i in range(len(df_list)):
+        if df_list[i].columns.shape == np_column.shape:
+            if np.all(df_list[i].columns == np_column):
+                df = df_list[i]
+
+    if remove_identicals:
+      df_with_NAN = Mask_Or_Drop_Successive_Identical_Values(df)
+    else:
+      df_with_NAN = df
+
+    column_names = ['Time'] + datacols_list
+
+    selected_rows = df_with_NAN[~df_with_NAN[datacols_list[0]].isnull()]
+    reduced_df_without_NAN = selected_rows[column_names]
+
+    return reduced_df_without_NAN.to_numpy()
+
+def ret_track_df(df):
+    # create an empty list to store DataFrames
+    dfs = []
+
+    # iterate over rows of the original DataFrame
+    for index, row in df.iterrows():
+        # split the bounding boxes string into individual boxes
+        bbs = row['bbs'].replace('[','').replace(']','').split(', ')
+        # create a DataFrame for each box and append it to the list of DataFrames
+        for bb in bbs:
+            bb_dict = {}
+            for item in bb.split("\n"):
+                key, value = item.split(": ")
+                bb_dict[key.strip()] = float(value) if key.strip() not in ["id", "Class"] else value.strip().replace('"', '')
+            new_row = {**row.to_dict(), **bb_dict}
+            dfs.append(pd.DataFrame(new_row, index=[index]))
+
+    # concatenate the list of DataFrames into a single DataFrame
+    new_df = pd.concat(dfs, axis=0, ignore_index=True)
+
+    new_df = new_df[["Time", "probability",	"xmin",	"ymin",	"xmax",	"ymax",	"id",	"Class"]]
+
+    new_df['id'] = new_df['id'].astype(int)
+
+    # extract the desired columns using loc
+    new_df = new_df.loc[:, ['Time', 'id', 'probability', 'Class', 'xmin', 'ymin', 'xmax', 'ymax']]
+
+    # calculate the center x and y coordinates
+    new_df['center_x'] = (new_df['xmin'] + new_df['xmax']) / 2
+    new_df['center_y'] = (new_df['ymin'] + new_df['ymax']) / 2
+
+    # drop the original xmin, ymin, xmax, ymax columns
+    new_df.drop(columns=['xmin', 'ymin', 'xmax', 'ymax'], inplace=True)
+
+    return new_df
+
+def remove_parentheses(string):
+    return string.replace('(', '').replace(')', '')
+
+# Define a function to extract the first three elements on the diagonal
+def extract_diagonal(row):
+    elements = row['pose.covariance'].split(',')
+    return pd.Series([float(remove_parentheses(elements[0])), float(elements[7]), float(remove_parentheses(elements[14]))])
